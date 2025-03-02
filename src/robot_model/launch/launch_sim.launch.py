9d84b58c86b735fdd519696ea5d94c3482264499
First Last <demo@example.com>
2025-03-03 03:47:05 +0700

    add gripperr

--- src/robot_model/launch/launch_sim.launch.py
+++ src/robot_model/launch/launch_sim.launch.py
@@ -1,27 +1,23 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
-
-    package_name='robot_model' 
-
+    # Package name
+    package_name = 'robot_model'  # Make sure this is your correct package name
+    
+    # Path to the parameter file for SLAM
     slam_params = os.path.join(
         get_package_share_directory(package_name), 
         'config', 
         'mapper_params_online_async.yaml'  # Ensure this file exists in the specified path
     )
 
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -32,26 +28,28 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'world': os.path.join(get_package_share_directory(package_name), 'world', 'map_b_red_block.world')}.items()
-             )#map_b_static.world
+                launch_arguments={'world': os.path.join(get_package_share_directory(package_name), 'world', 'map_b_red_block.world')}.items()  # Update to use your world file
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # Run the spawner node from the gazebo_ros package
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot',
-                                   '-x', '0', '-y', '0', '-z', '0.8',  
-                                   '-R', '0', '-P', '0', '-Y', '1.57', 
+                                   '-x', '0', '-y', '0', '-z', '0.8',  # Set initial point
+                                   '-R', '0', '-P', '0', '-Y', '1.57', # Set Rotage
                                    ],
                         output='screen')
-    
+
+    # Launch SLAM tool with parameters from the provided YAML file
     slam = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('slam_toolbox'),'launch','online_async_launch.py'
                 )]), 
                 launch_arguments={'use_sim_time': 'true', 'params_file': slam_params}.items()
     )
+    
 
-
+    # Node for twist_mux
     twist_mux = Node(
         package='twist_mux',
         executable='twist_mux',
@@ -67,7 +65,8 @@ def generate_launch_description():
             ('cmd_vel_out', 'diff_cont/cmd_vel_unstamped')  # Remap output topic
         ]
     )
-
+    
+    
     controller_manager = Node(
         package="controller_manager",
         executable="spawner",
@@ -77,7 +76,6 @@ def generate_launch_description():
         ],
     )
     
-
     # Launch them all!
     return LaunchDescription([
         rsp,
--- src/robot_model/launch/rsp.launch.py
+++ src/robot_model/launch/rsp.launch.py
@@ -28,14 +28,14 @@ def generate_launch_description():
         output='screen',
         parameters=[params]
     )
-
-    # # Create a joint_state_publisher node
-    # node_joint_state_publisher_gui = Node(
-    #     package='joint_state_publisher_gui',
-    #     executable='joint_state_publisher_gui',
-    #     output='screen',
-    #     parameters=[params]
-    # )
+    
+    # Create a joint_state_publisher node
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        parameters=[params]
+    )
     
     node_joint_state_publisher = Node(
         package='joint_state_publisher',
@@ -44,7 +44,7 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    rviz_config_file = os.path.join(pkg_path, 'config', 'test.rviz')
+    rviz_config_file = os.path.join(pkg_path, 'config', 'main.rviz')
     node_rviz2 = Node(
         package='rviz2',
         executable='rviz2',
@@ -52,7 +52,6 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', rviz_config_file]
     )
-
 
     # Launch!
     return LaunchDescription([
@@ -62,6 +61,7 @@ def generate_launch_description():
             description='Use sim time if true'),
 
         node_robot_state_publisher,
-        node_rviz2,
-        node_joint_state_publisher
+        # node_joint_state_publisher_gui,
+        node_joint_state_publisher,
+        # node_rviz2
     ])
